45c6674ef9eb0a8a3b6a07c6c936b7177b507f62
szatro123 <106858805+szatro123@users.noreply.github.com>
2026-04-05 17:04:48 +0200

    Update add-alert.js

--- api/add-alert.js
+++ api/add-alert.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   return res.status(200).json({
     ok: true,
-    message: "api działa"
+    supabaseUrl: process.env.SUPABASE_URL || null,
+    hasSupabaseKey: !!process.env.SUPABASE_KEY
   });
 }
